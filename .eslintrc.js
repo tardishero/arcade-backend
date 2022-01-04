b71fdc9da8e63a4699db4405f1cb3b868c051eb5
zeno3618 <zeno3618@gmail.com>
2022-01-04 18:59:00 +1000

    env: eslint, prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier',
-    'prettier/@typescript-eslint'
   ],
   root: true,
   env: {
@@ -26,12 +25,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': 'error',
-    'semi': ['error', 'always'],
+    'prettier/prettier': ['error', {'semi': true}],
     'max-len': 0,
     '@typescript-eslint/require-await': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
     '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/no-misused-promises': 'off'
+    '@typescript-eslint/no-misused-promises': 'off',
+    'semi': ['error', 'always']
   },
 };
